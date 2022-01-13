3d21c50c19defd4dfb5ea55bc6770957297b323a
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2022-01-13 10:55:05 +0100

    Changed trigonometric functions to fixed point

--- tlvsi.c
+++ tlvsi.c
@@ -13,6 +13,13 @@
 #include "tptransforms.h"
 #include <math.h>
 
+#include <stdbool.h>
+//#include <assert.h>
+
+int64_t sine(uint64_t value);
+int64_t cosine(uint64_t value);
+int64_t clamp_overflow(int64_t value, int width);
+
 //===========================================================================
 
 #define TLVSI_CONFIG_Li			((float)3.4e-3)
@@ -170,7 +177,7 @@ uint32_t tlvsiOptFixed(psdtypesABCint_t *ii, psdtypesABCint_t *ig, psdtypesABCin
 			.spll_3ph_1 = {.v_q = {0, 0}, .ylf = {0, 0}, .fo = 0, .fn = fixedmathftoi(50.0f), .theta = {0, 0}, .delta_t = fixedmathftoi(TLVSI_CONFIG_ts), .lpf_coeff = {.b0 = fixedmathftoi(166.877556f), .b1 = fixedmathftoi(-166.322444f)}}
 	};
 
-	float theta, phi;
+	fmint_t theta, phi;
 	fmint_t J, Jk;
 	fmint_t co, si;
     uint32_t k;
@@ -178,8 +185,11 @@ uint32_t tlvsiOptFixed(psdtypesABCint_t *ii, psdtypesABCint_t *ig, psdtypesABCin
     fmint_t ii_d_constant, ii_q_constant, ig_d_constant, ig_q_constant, vc_d_constant, vc_q_constant;
 
     /* Pre-computes sin and cos for DQ0 transforms */
-    si = fixedmathftoi(sinf(vsi.theta));
-    co = fixedmathftoi(cosf(vsi.theta));
+    co = cosine((uint64_t)(fixedmul(vsi.spll_3ph_1.theta[1], (fixedmathftoi(0.15915494309189535f))) >> (FIXED_MATH_Q - 20)));
+    co = co << (FIXED_MATH_Q - 18 + 1);
+
+    si = sine((uint64_t)(fixedmul(vsi.spll_3ph_1.theta[1], (fixedmathftoi(0.15915494309189535f))) >> (FIXED_MATH_Q - 20)));
+    si = si << (FIXED_MATH_Q - 18 + 1);
 
     tptransformsABCDQ0Int(ii, &vsi.ii_d, si, co);
     tptransformsABCDQ0Int(ig, &vsi.ig_d, si, co);
@@ -187,7 +197,7 @@ uint32_t tlvsiOptFixed(psdtypesABCint_t *ii, psdtypesABCint_t *ig, psdtypesABCin
     tptransformsABCDQ0Int(vg, &vsi.vg_k, si, co);
 
     tppllRunInt(vsi.vg_k.q, &vsi.spll_3ph_1);
-    vsi.theta = fixedmathitof(vsi.spll_3ph_1.theta[1]);
+    vsi.theta = vsi.spll_3ph_1.theta[1];
 
     /* Delay compensation */
     tlvsiPredictFixed(&vsi.ii_k, &vsi.ii_d, &vsi.ig_k, &vsi.ig_d,
@@ -247,9 +257,13 @@ uint32_t tlvsiOptFixed(psdtypesABCint_t *ii, psdtypesABCint_t *ig, psdtypesABCin
     phi = 0;
     for(k = 1; k < 7; k++){
 
-        theta = vsi.theta - phi;
-        co = fixedmathftoi( cosf(theta) );
-        si = fixedmathftoi( sinf(theta) );
+        theta = vsi.spll_3ph_1.theta[1] - phi;
+
+        co = cosine((uint64_t)(fixedmul(theta, (fixedmathftoi(0.15915494309189535f))) >> (FIXED_MATH_Q - 20)));
+        co = co << (FIXED_MATH_Q - 18 + 1);
+
+        si = sine((uint64_t)(fixedmul(theta, (fixedmathftoi(0.15915494309189535f))) >> (FIXED_MATH_Q - 20)));
+        si = si << (FIXED_MATH_Q - 18 + 1);
 
         co = fixedmul(TLVSI_CONFIG_k3_int, co);
         si = fixedmul(TLVSI_CONFIG_k3_int, si);
@@ -276,7 +290,7 @@ uint32_t tlvsiOptFixed(psdtypesABCint_t *ii, psdtypesABCint_t *ig, psdtypesABCin
             vsi.sw = k;
         }
 
-        phi += 1.0471975511965976f;
+        phi += fixedmathftoi(1.0471975511965976f);
     }
 
     if( Jopt != 0 ) *Jopt = J;
@@ -309,7 +323,7 @@ void tlvsiPredictFixed(psdtypesDQ0int_t *ii_k_1, psdtypesDQ0int_t *ii_k,
 				  	   psdtypesDQ0int_t *ig_k_1, psdtypesDQ0int_t *ig_k,
 					   psdtypesDQ0int_t *vc_k_1, psdtypesDQ0int_t *vc_k,
 					   psdtypesDQ0int_t *vg_k,
-					   float theta, uint32_t sw){
+					   fmint_t theta, uint32_t sw){
 
 	fmint_t co, si;
 
@@ -317,10 +331,13 @@ void tlvsiPredictFixed(psdtypesDQ0int_t *ii_k_1, psdtypesDQ0int_t *ii_k,
 	ii_k_1->q = ii_k->q - fixedmul(TLVSI_CONFIG_k1_int, ii_k->d) + fixedmul(TLVSI_CONFIG_k2_int, vc_k->q);
 	if(sw != 0){
 
-		theta = theta - (sw-1)*1.0471975511965976f;
+		theta = theta - fixedmul( ( (sw-1) << FIXED_MATH_Q ), ( fixedmathftoi(1.0471975511965976f) ) );
 
-		co = fixedmathftoi( cosf(theta) );
-        si = fixedmathftoi( sinf(theta) );
+        co = cosine((uint64_t)(fixedmul(theta, (fixedmathftoi(0.15915494309189535f))) >> (FIXED_MATH_Q - 20)));
+        co = co << (FIXED_MATH_Q - 18 + 1);
+
+        si = sine((uint64_t)(fixedmul(theta, (fixedmathftoi(0.15915494309189535f))) >> (FIXED_MATH_Q - 20)));
+        si = si << (FIXED_MATH_Q - 18 + 1);
 
         co = fixedmul(TLVSI_CONFIG_k3_int, co);
         si = fixedmul(TLVSI_CONFIG_k3_int, si);
@@ -392,3 +409,147 @@ fmint_t tlvsiCostFixed(psdtypesDQ0int_t *ii, psdtypesDQ0int_t *ii_ref,
 //---------------------------------------------------------------------------
 //===========================================================================
 
+/*
+ * Implementation of the fixed-point sine and cosine functions were extracted
+ * from: https://github.com/dsnet/tri-approx
+ *
+ * There were only very small changes.
+ */
+
+// Fixed-point sine approximation. Normalized for an input domain of [0,1)
+// instead of the usual domain of [0,2*PI).
+//
+// Uses Taylor series approximation for sine centered at zero:
+//  sine(2*PI*x) = 0 + (2*PI*x)^1/1! - (2*PI*x)^3/3!
+//                   + (2*PI*x)^5/5! - (2*PI*x)^7/7!
+//               = k_1*x^1 - k_3*x^3 + k_5*x^5 - k_7*x^7
+//
+// The bit-width of 18 appears often because it is the width of hardware
+// multipliers on Altera FPGAs.
+//
+// Input: 20-bit unsigned fixed point integer upscaled by 2^20
+// Output: 18-bit two's complement fixed point integer upscaled by 2^17
+int64_t sine(uint64_t value) {
+    // These are polynomial constants generated for each term in the Taylor
+    // series. They have been upscaled to the largest value that fits within
+    // 18-bits for greatest precision. The constants labeled with [ADJ] have
+    // been manually adjusted to increase accuracy.
+    const uint64_t k1 = 205887; // k1 = round((2*PI)^1/1! * 2^15)
+    const uint64_t k3 = 169336; // k3 = round((2*PI)^3/3! * 2^12)
+    const uint64_t k5 = 167014; // k5 = round((2*PI)^5/5! * 2^11) [ADJ]
+    const uint64_t k7 = 150000; // k7 = round((2*PI)^7/7! * 2^11) [ADJ]
+
+    // Uses symmetric properties of sine to get more accurate results
+    // Normalize the x value to a 18-bit value upscaled by 2^20
+    bool high0 = ((value >> 19) & 0x01);
+    bool high1 = ((value >> 18) & 0x01);
+    uint64_t x1 = value & 0x3ffff; // Strip two highest bits
+    if (high1) {
+        x1 = (((uint64_t)0x1 << 18) - x1) & 0x3ffff;
+    }
+    bool negative = high0;
+    bool one = (x1 == 0) && high1;
+
+    // Compute the power values (most of these must be done in series)
+    uint64_t x2 = ((x1 * x1) >> 18); // Scale: 2^22
+    uint64_t x3 = ((x2 * x1) >> 18); // Scale: 2^24
+    uint64_t x5 = ((x2 * x3) >> 18); // Scale: 2^28
+    uint64_t x7 = ((x2 * x5) >> 18); // Scale: 2^32
+
+    // Compute the polynomial values (these can be done in parallel)
+    uint64_t kx1 = ((k1 * x1) >> 17); // Scale: 2^18
+    uint64_t kx3 = ((k3 * x3) >> 18); // Scale: 2^18
+    uint64_t kx5 = ((k5 * x5) >> 21); // Scale: 2^18
+    uint64_t kx7 = ((k7 * x7) >> 25); // Scale: 2^18
+
+    // Add all the terms together (these can be done in series-parallel)
+    int64_t sum = kx1 - kx3 + kx5 - kx7; // Scale: 2^18
+    sum = sum >> 1; // Scale: 2^17
+
+    // Perform reflection math and corrections
+    if (one) { // Check if sum should be one
+        sum = (1 << 17);
+    }
+    if (negative) { // Check if the sum should be negative
+        sum = ~sum + 1;
+    }
+    return clamp_overflow(sum, 18);
+}
+
+// Fixed-point cosine approximation. Normalized for an input domain of [0,1)
+// instead of the usual domain of [0,2*PI).
+//
+// Uses Taylor series approximation for cosines centered at zero:
+//  cosine(2*PI*x) = 1 - (2*PI*x)^2/2! + (2*PI*x)^4/4!
+//                     - (2*PI*x)^6/6! + (2*PI*x)^8/8!
+//                 = 1 - k_2*x^2 + k_4*x^4 - k_6*x^6 + k_8*x^8
+//
+// The bit-width of 18 appears often because it is the width of hardware
+// multipliers on Altera FPGAs.
+//
+// Input: 20-bit unsigned fixed point integer upscaled by 2^20
+// Output: 18-bit two's complement fixed point integer upscaled by 2^17
+int64_t cosine(uint64_t value) {
+    // These are polynomial constants generated for each term in the Taylor
+    // series. They have been upscaled to the largest value that fits within
+    // 18-bits for greatest precision. The constants labeled with [ADJ] have
+    // been manually adjusted to increase accuracy.
+    const uint64_t k2 = 161704; // k2 = round((2*PI)^2/2! * 2^13)
+    const uint64_t k4 = 132996; // k4 = round((2*PI)^4/4! * 2^11)
+    const uint64_t k6 = 175016; // k6 = round((2*PI)^6/6! * 2^11)
+    const uint64_t k8 = 241700; // k8 = round((2*PI)^8/8! * 2^12) [ADJ]
+
+    // Uses symmetric properties of cosine to get more accurate results
+    // Normalize the x value to a 18-bit value upscaled by 2^20
+    bool high0 = ((value >> 19) & 0x01);
+    bool high1 = ((value >> 18) & 0x01);
+    uint64_t x1 = value & 0x3ffff; // Strip two highest bits
+    if (high1) {
+        x1 = (((uint64_t)0x1 << 18) - x1) & 0x3ffff;
+    }
+    bool negative = high0 ^ high1;
+    bool zero = (x1 == 0) && high1;
+
+    // Compute the power values (most of these must be done in series)
+    uint64_t x2 = ((x1 * x1) >> 18); // Scale: 2^22
+    uint64_t x4 = ((x2 * x2) >> 18); // Scale: 2^26
+    uint64_t x6 = ((x4 * x2) >> 18); // Scale: 2^30
+    uint64_t x8 = ((x4 * x4) >> 18); // Scale: 2^34
+
+    // Compute the polynomial values (these can be done in parallel)
+    uint64_t kx2 = ((k2 * x2) >> 17); // Scale: 2^18
+    uint64_t kx4 = ((k4 * x4) >> 19); // Scale: 2^18
+    uint64_t kx6 = ((k6 * x6) >> 23); // Scale: 2^18
+    uint64_t kx8 = ((k8 * x8) >> 28); // Scale: 2^18
+
+    // Add all the terms together (these can be done in series-parallel)
+    int64_t sum = ((int64_t)1 << 18) - kx2 + kx4 - kx6 + kx8; // Scale: 2^18
+    sum = sum >> 1; // Scale: 2^17
+
+    // Perform reflection math and corrections
+    if (zero) { // Check if sum should be zero
+        sum = 0;
+    }
+    if (negative) { // Check if the sum should be negative
+        sum = ~sum + 1;
+    }
+    return clamp_overflow(sum, 18);
+}
+
+// Clamp an overflowed fixed-point two's complement value.
+int64_t clamp_overflow(int64_t value, int width) {
+    bool high0 = ((value >> (width+0)) & 0x01);
+    bool high1 = ((value >> (width-1)) & 0x01);
+    if (high0 ^ high1) {
+        if (high0) {
+            value = (-1 << (width-1));
+        } else {
+            value = (1 << (width-1)) - 1;
+        }
+    }
+
+//    // Sanity check to ensure there is no overflow
+//    int64_t high = value >> (width-1);
+//    assert((high == 0x0000000000000000LL) || (high == 0xFFFFFFFFFFFFFFFFLL));
+    return value;
+}
--- tlvsi.h
+++ tlvsi.h
@@ -100,7 +100,7 @@ void tlvsiPredictFixed(psdtypesDQ0int_t *ii_k_1, psdtypesDQ0int_t *ii_k,
 				  	   psdtypesDQ0int_t *ig_k_1, psdtypesDQ0int_t *ig_k,
 					   psdtypesDQ0int_t *vc_k_1, psdtypesDQ0int_t *vc_k,
 					   psdtypesDQ0int_t *vg_k,
-					   float theta, uint32_t sw);
+					   fmint_t theta, uint32_t sw);
 
 uint32_t tlvsiOptFixed(psdtypesABCint_t *ii, psdtypesABCint_t *ig, psdtypesABCint_t *vc, psdtypesABCint_t *vg, fmint_t *Jopt);
 
